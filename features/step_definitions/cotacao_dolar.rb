df7b945590abe483be32ace5c1ab961d9a714646
Esther Hinrichsen <estherhinrichsen@MacBook-Pro-de-Esther.local>
2017-08-29 17:00:05 -0300

    Conversão de string para float

--- features/step_definitions/cotacao_dolar.rb
+++ features/step_definitions/cotacao_dolar.rb
@@ -5,15 +5,17 @@ end
 
 Quando(/^acesso a página de economia$/) do
  #Acessa o link de economia
- click_on "Economia"
+ #click_on "Economia"
+ click_link('Economia')
 end
 
 Então(/^verifico se o valor do dólar está abaixo de (\d+.\d+)$/) do |dolar|
   #pega o texto da área de cotação
-  cotacao = find(:xpath,".//*[@id='cambio']/ul/li[1]/p[2]").text
-
+  cotacao = find(:xpath,".//*[@id='cambio']/ul/li[1]/p[2]").text.strip
+  inicio = cotacao.index('$') 
+  cotacao = cotacao[inicio, cotacao.length]
   #Pega apenas a parte da string que corresponde a cotação do dólar e converte para float
-  cotacao = (cotacao[-5..-1].sub(",", ".")).to_f
+  cotacao = '%.2f' % cotacao.delete( "$" ).to_f
 
   #Se o valor do dólar estiver maior que 3,20 falha o teste
   if cotacao > dolar
